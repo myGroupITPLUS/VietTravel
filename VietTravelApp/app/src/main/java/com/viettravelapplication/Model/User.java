b1e8e972826f6df7591edb18943e88c9737caf68
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-11-28 13:30:59 +0700

    complete

--- VietTravelApp/app/src/main/java/com/viettravelapplication/Model/User.java
+++ VietTravelApp/app/src/main/java/com/viettravelapplication/Model/User.java
@@ -4,13 +4,22 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String email;
     private int quyen;
 
-    public User(String username, String password, int quyen) {
+    public User(String username, String password, int quyen, String email) {
 //        this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
         this.quyen = quyen;
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.quyen = 1;
     }
 
     public User(String username, String password) {
@@ -48,6 +57,14 @@ public class User {
 
     public void setQuyen(int quyen) {
         this.quyen = quyen;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
